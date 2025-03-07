971585f13a00cd86e72f41f7b1c2880ec2b7d0d1
Francesc Esplugas <2720+fesplugas@users.noreply.github.com>
2025-03-07 20:15:12 +0100

    updates db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_07_190715) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_07_191329) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.boolean "published"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
